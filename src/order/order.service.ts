d7fca5ef37d537aecb50a66e192b03890ea3d5e8
terry <terry@w3company.team>
2023-11-26 11:22:40 +0900

    feat: order 정보에 store 정보 추가

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -35,7 +35,7 @@ export class OrderService {
         { user: { id: userId }, reserv: 0, status: Not(0) },
         { user: { id: userId }, reserv: 1, status: Not(0) },
       ],
-      relations: { reviews: true },
+      relations: { reviews: true, store: true },
     });
   }
 
